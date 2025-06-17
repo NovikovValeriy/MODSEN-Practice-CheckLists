aa7c55bb0d00e12c99d43972485883b889435693
NovikovValeriy <novikov.valera2005@gmail.com>
2025-06-17 21:05:28 +0300

    Initial Commit

---
+++ CheckLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckLists
+//
+//  Created by Валерий Новиков on 17.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
